8efbd22f32d1dc661ca346faee82c51f0aad4746
Lazaroscuro <endoflinelogic@gmail.com>
2018-02-15 15:07:43 +0100

    Add a header for foo

--- CI_Test.c
+++ CI_Test.c
@@ -1,12 +1,11 @@
+#include "foo.h"
 #include <stdio.h>
 
 int
 main(int argc, char **argv)
 {
-	/*
 	if(argc > 1)
 		fprintf(stderr, "usage: %s\n", argv[0]);
-	*/
 
 	printf("Hello, world!\n");
 	printf("System check: %d\n", foo());
